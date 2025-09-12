376d7aec2ef11f64da846930997433ba62039cb0
Luke Lombardi <luke@beam.cloud>
2025-09-12 15:13:26 -0400

    add client

--- pkg/client.go
+++ pkg/client.go
@@ -2,20 +2,38 @@ package goproc
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/beam-cloud/goproc/proto"
+	"google.golang.org/grpc"
 )
 
 type GoProcClient struct {
-	ctx context.Context
+	ctx    context.Context
+	port   uint
+	conn   *grpc.ClientConn
+	client proto.GoProcClient
 }
 
-func NewGoProcClient(ctx context.Context, cfg GoProcConfig) (*GoProcClient, error) {
+func NewGoProcClient(ctx context.Context, port uint) (*GoProcClient, error) {
 	c := &GoProcClient{
-		ctx: ctx,
+		ctx:  ctx,
+		port: port,
 	}
 
+	conn, err := grpc.NewClient(fmt.Sprintf("0.0.0.0:%d", port), grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	c.conn = conn
+	c.client = proto.NewGoProcClient(conn)
 	return c, nil
 }
 
 func (c *GoProcClient) Cleanup() error {
+	if c.conn != nil {
+		return c.conn.Close()
+	}
+
 	return nil
 }
